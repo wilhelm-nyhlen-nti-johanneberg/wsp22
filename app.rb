b883ac118d52e7fe106508b3d491a9441de6cd33
wilhelm-nyhlen-nti-johanneberg <74763434+wilhelm-nyhlen-nti-johanneberg@users.noreply.github.com>
2022-04-24 18:08:13 +0200

    24 - 1
    Lite ändringar, främst i app(har försökt fixa registrering utav användare, inte riktigt lyckats än).

--- app.rb
+++ app.rb
@@ -4,3 +4,42 @@ require 'byebug'
 require 'sqlite3'
 require 'bcrypt'
 require 'sinatra/reloader'
+
+enable :sessions
+
+
+
+def connect_database(path)
+  db = SQLite3::Database.new(path)
+  db.results_as_hash = true
+  return db
+end
+
+get("/users/new") do
+  slim(:"users/new")
+end
+
+post("/users/new") do 
+  username = params[:username]
+  password = params[:password]
+  password_confirm = params[:password_confirm]
+
+  if password == password_confirm
+    db = SQLite3::Database.new("db/wspdatabase.db")
+    db.execute("INSERT INTO User (user_name, password) VALUES (?,?)", username, password)
+    redirect("/")
+  else 
+    p "Passwords did not match. Please try again."
+
+  end
+
+end
+
+
+
+get('/')  do
+    slim(:posts)
+end 
+
+
+  
